aa91ef35596df9fea1eb65c1b93e03ef8db5e950
Ludovic Espeel <ludovic.espeel>
2023-11-14 14:48:16 +0100

    add example chapter 8: RuntimeExceptions

---
+++ H8_ContentProvider/RuntimePermissions/app/src/main/java/com/example/runtimepermissions/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.runtimepermissions
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.runtimepermissions.databinding.ActivityMainBinding
+import android.Manifest.permission.CALL_PHONE
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.makeCallButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(CALL_PHONE), 1)
+            } else {
+                call()
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:1933")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+}
