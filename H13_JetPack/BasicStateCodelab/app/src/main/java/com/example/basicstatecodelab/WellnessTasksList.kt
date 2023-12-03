7ba62640cb249a3f5b9d98aa35c23681b6fa3d28
Ludovic Espeel <ludovic.espeel>
2023-12-03 16:36:26 +0100

    rename folders

---
+++ H13_JetPack/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
@@ -0,0 +1,28 @@
+package com.example.basicstatecodelab
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    onCloseTask: (WellnessTask) -> Unit,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
+    list: List<WellnessTask>
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(items = list,
+        key = { task -> task.id})
+        { task ->
+            WellnessTaskItem(taskName = task.label, checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },onClose = { onCloseTask(task)})
+        }
+    }
+}
